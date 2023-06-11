e7922f4febf1a3c78710c1b295730d5087dcd332
sasha wangui <sasha.iluku@gmail.com>
2023-06-12 01:40:45 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+let body=document.body
+let main = document.getElementById("main");
+
+body.removeChild(main);
+
+let newHeader =document.createElement('h1');
+newHeader.id="victory";
+newHeader.innerHTML="Natalie is the champion"
+body.appendChild(newHeader)
